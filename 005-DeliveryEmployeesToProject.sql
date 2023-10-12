e5a47450c4ee4be6c06fb678bbbb203fecdb531d
Darragh <darragh.haughey@kainos.com>
2023-10-12 15:56:14 +0100

    Removed old file, updated 005

--- 005-DeliveryEmployeesToProject.sql
+++ 005-DeliveryEmployeesToProject.sql
@@ -1,21 +1,37 @@
 DELIMITER $$
-DROP PROCEDURE IF EXISTS my_transaction $$
-CREATE PROCEDURE my_transaction ()
+DROP PROCEDURE IF EXISTS my_delivery_project_transaction $$
+CREATE PROCEDURE my_delivery_project_transaction ()
 BEGIN
 	START TRANSACTION;
 
+	CREATE TABLE DeliveryProject
+	(
+		DeliveryProjectID SMALLINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+		TechLead BOOLEAN,
+		ActiveOnProject BOOLEAN,
+		EmployeeID SMALLINT UNSIGNED,
+		ProjectID SMALLINT UNSIGNED,
+        
+        CONSTRAINT fk_DeliveryProject_EmployeeID
+		FOREIGN KEY(EmployeeID)
+		REFERENCES Employee(EmployeeID),
+        
+        CONSTRAINT fk_DeliveryProject_ProjectID
+		FOREIGN KEY(ProjectID)
+		REFERENCES Project(ProjectID)
+	);
+    
+	-- Sample data to assign a delivery employee (employee_id = 1) to a project (project_id = 1)
+	INSERT INTO DeliveryEmployeeProjects (delivery_employee_id, project_id)
+	VALUES (1, 1);
 
--- Sample data to assign a delivery employee (employee_id = 1) to a project (project_id = 1)
-INSERT INTO DeliveryEmployeeProjects (delivery_employee_id, project_id)
-VALUES (1, 1);
+	-- Sample data to assign the same delivery employee (employee_id = 1) to the same project (project_id = 1) again
+	INSERT INTO DeliveryEmployeeProjects (delivery_employee_id, project_id)
+	VALUES (1, 1);
 
--- Sample data to assign the same delivery employee (employee_id = 1) to the same project (project_id = 1) again
-INSERT INTO DeliveryEmployeeProjects (delivery_employee_id, project_id)
-VALUES (1, 1);
-
--- Sample data to assign a different delivery employee (employee_id = 2) to a different project (project_id = 2)
-INSERT INTO DeliveryEmployeeProjects (delivery_employee_id, project_id)
-VALUES (2, 2);
+	-- Sample data to assign a different delivery employee (employee_id = 2) to a different project (project_id = 2)
+	INSERT INTO DeliveryEmployeeProjects (delivery_employee_id, project_id)
+	VALUES (2, 2);
 
 	-- check the number of affected rows
 	GET DIAGNOSTICS @rows = ROW_COUNT;
