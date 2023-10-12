161e41873e82c3009489eb37629b9f090d8595fe
Jonny Evans <jonathan-evans99@hotmail.com>
2023-10-12 14:40:00 +0100

    010-MAXVALUE

---
+++ 010-MaxValue.sql
@@ -0,0 +1,28 @@
+DELIMITER $$
+DROP PROCEDURE IF EXISTS my_transaction $$
+CREATE PROCEDURE my_transaction ()
+BEGIN
+	START TRANSACTION;
+
+-- Getting client with max project value
+
+-- GRANT PERMISSION to 'SalesEmployees'
+
+SELECT
+    Client.ClientID,
+    CONCAT(Client.ClientFirstName, ' ', Client.ClientSurname) AS ClientName,
+    SUM(Project.ProjectValue) AS TotalProjectValue
+FROM
+    Clients
+LEFT JOIN
+    Projects ON Client.ClientID = Project.ClientID
+GROUP BY
+    Client.ClientID, ClientName
+ORDER BY
+    TotalProjectValue DESC
+
+LIMIT 1
+
+END $$
+DELIMITER ;
+CALL my_transaction();
