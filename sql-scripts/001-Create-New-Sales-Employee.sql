e81902fe2fdd1661b4f5e1f3fea162c9ce0e3ca3
Darragh <darragh.haughey@kainos.com>
2023-10-12 14:25:43 +0100

    Create new sales employee SQL file

--- sql-scripts/001-Create-New-Sales-Employee.sql
+++ sql-scripts/001-Create-New-Sales-Employee.sql
@@ -1,16 +1,48 @@
 DELIMITER $$
-DROP PROCEDURE IF EXISTS my_transaction $$
-CREATE PROCEDURE my_transaction ()
+DROP PROCEDURE IF EXISTS my_sales_employee_transaction $$
+CREATE PROCEDURE my_sales_employee_transaction()
 BEGIN
-	START TRANSACTION;
+
+	CREATE TABLE Employee
+	(	
+		EmployeeID SMALLINT UNSIGNED PRIMARY KEY AUTO_INCREMENT, 
+		EmployeeFirstName VARCHAR(20),
+		EmployeeSurname VARCHAR(20),
+		Salary DECIMAL(10,2),
+		BankAccountNo VARCHAR(64),
+		NationalInsuranceNo VARCHAR(64),
+		JobTitleID TINYINT
+	);
+
+	CREATE TABLE JobTitle
+	(
+		JobTitleID TINYINT PRIMARY KEY AUTO_INCREMENT,
+		JobTitleName VARCHAR (20)
+	);
+
+	CREATE TABLE SalesEmployee
+	(
+		SalesEmployeeID SMALLINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+		JobTitleName VARCHAR (20),
+		EmployeeEmployeeID SMALLINT UNSIGNED
+	);
+
+    ALTER TABLE Employee
+	ADD CONSTRAINT fk_Employee_JobTitleID
+	FOREIGN KEY(JobTitleID)
+	REFERENCES JobTitle(JobTitleID);
     
-    -- As a member of the HR team I want to be able to create a new sales employee. 
+    ALTER TABLE SalesEmployee
+	ADD CONSTRAINT fk_SalesEmployee_EmployeeID
+	FOREIGN KEY(EmployeeID)
+	REFERENCES Employee(EmployeeID);
+    
+	-- As a member of the HR team I want to be able to create a new sales employee. 
     -- I should be able to store a name, salary, bank account number, national insurance number and commission rate
     
-    
     -- 1. Populate the Employee table using an insert
-     INSERT INTO Employee (EmployeeID, EmployeeFistName, EmployeeSurname, Salary, BankAccountNumber, NationalInsuranceNumber, JobTitleID) 
-     VALUES ( , '', '', , , , );
+     INSERT INTO Employee (EmployeeFistName, EmployeeSurname, Salary, BankAccountNumber, NationalInsuranceNumber, JobTitleID) 
+     VALUES ('', '', , , , 1);
     
     -- check the number of affected rows
 	GET DIAGNOSTICS @rows = ROW_COUNT;
@@ -22,12 +54,11 @@ BEGIN
 		--  If no error occurred, commit transaction
 		COMMIT;
 		SELECT 'Transaction committed successfully';
-	 END IF;
-     
+	END IF;
      
     -- 2. The employee exists and their comission can be exclusively applied to them in the SalesEmployee table
-    INSERT INTO SalesEmployee (EmployeeID, CommissionRate) 
-     VALUES ( , );
+    INSERT INTO SalesEmployee (CommissionRate) 
+	VALUES ( );
     
     -- check the number of affected rows
 	GET DIAGNOSTICS @rows = ROW_COUNT;
@@ -40,7 +71,6 @@ BEGIN
 		COMMIT;
 		SELECT 'Transaction committed successfully';
 	 END IF;    
-    
-    END $$
+END $$
 DELIMITER ;
-CALL my_transaction();
+CALL my_sales_employee_transaction();
