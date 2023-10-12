7c7420ea2946fa6a2f2ba200270af955e50cae50
Darragh <darragh.haughey@kainos.com>
2023-10-12 14:50:45 +0100

    Updating 002

--- 002-Create-New_delivery-Employee.sql
+++ 002-Create-New_delivery-Employee.sql
@@ -1,12 +1,13 @@
 DELIMITER $$
-DROP PROCEDURE IF EXISTS my_transaction $$
-CREATE PROCEDURE my_transaction ()
+DROP PROCEDURE IF EXISTS my_delivery_employee_transaction $$
+CREATE PROCEDURE my_delivery_employee_transaction ()
 BEGIN
 	START TRANSACTION;
+	-- Grant Access
         
     -- 1. Populate the Employee table using an insert, use the corresponding job titleID for a delivery employee
-     INSERT INTO Employee (EmployeeID, EmployeeFistName, EmployeeSurname, Salary, BankAccountNumber, NationalInsuranceNumber, JobTitleID) 
-     VALUES ( , '', '', , , , );
+     INSERT INTO Employee (EmployeeFirstName, EmployeeSurname, Salary, BankAccountNo, NationalInsuranceNo, JobTitleID) 
+     VALUES ('Jonathan', 'Evans', 35000.00, "2345678901", "CD123456E", 2);
     
     -- check the number of affected rows
 	GET DIAGNOSTICS @rows = ROW_COUNT;
